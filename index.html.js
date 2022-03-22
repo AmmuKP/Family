b0bbfa202cc91f3056d1a6cbe8eb9909acfbedce
AmmuKP <96097575+AmmuKP@users.noreply.github.com>
2022-03-22 15:36:13 +0530

    Add files via upload

---
+++ index.html.js
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>My First Console App</title>
+    </head>
+    <body>
+        <h1>My First Console App</h1>
+        <p>Write about visual studio and how to set it up.</p>
+        <p>Write about console screen and about how it is used. And how to open console screen and how to print anything on console.</p>
+        var first_name = "Krithika";
+        var last_name = "Prakash";
+        var a = 10;
+        var b = 5;
+        var c = 20;
+        var d = 7;
+        var add = a + b;
+        var ans = c + d;
+        <script>
+            first_name.concat(last_name);
+            console.log(add);
+            console.log(ans);            
+        </script>
+    </body>
+</html>
